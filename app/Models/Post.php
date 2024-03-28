07ab5b1f753971d93a9ce84c600543cd936cc031
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-03-28 23:09:41 +0200

    chore(marwan): Add Comment, Tag, and defined relationships between models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
+use Egulias\EmailValidator\Parser\Comment;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likedByUsers()
+    {
+        return $this->belongsToMany(User::class, 'likes');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'post_hashtags');
+    }
 }
