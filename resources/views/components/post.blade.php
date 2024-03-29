6da0adb9fb3b0a4dfa8db010b54a2b7ed5ff1a93
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-03-30 00:19:54 +0200

    feat: Add Post component and post.blade.php template

---
+++ resources/views/components/post.blade.php
@@ -0,0 +1,168 @@
+<div>
+  <div class="post">
+    <div class="info">
+      <div class="person" data-bs-toggle="modal" data-bs-target="#post-modal">
+        <img src="https://i.ibb.co/3S1hjKR/account1.jpg">
+        <a href="#">zineb</a>
+        <span class="circle">.</span>
+        <a>45m</a>
+      </div>
+      <div class="more">
+        <img src="./images/show_more.png" alt="show more">
+      </div>
+    </div>
+    <div class="image">
+      <img src="https://i.ibb.co/Jqh3rHv/img1.jpg">
+    </div>
+    <div class="desc">
+      <div class="icons">
+        <div class="icon_left d-flex">
+          <div class="like">
+            <img class="not_loved" src="./images/love.png">
+            <img class="loved" src="./images/heart.png">
+          </div>
+          <div class="chat">
+            <button type="button" class="btn p-0" data-bs-toggle="modal" data-bs-target="#post-modal">
+              <img src="./images/bubble-chat.png">
+            </button>
+          </div>
+          <div class="send">
+            <button type="button" class="btn p-0" data-bs-toggle="modal" data-bs-target="#send_message_modal">
+              <img src="./images/send.png">
+            </button>
+          </div>
+        </div>
+        <div class="save not_saved">
+          <img class="hide saved" src="./images/save_black.png">
+          <img class="not_saved" src="./images/save-instagram.png">
+        </div>
+      </div>
+      <div class="liked">
+        <a class="bold" href="#">150 likes</a>
+      </div>
+      <div class="post_desc">
+        <p>
+          <a class="bold" href="#">zineb</a>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima accusantium aperiam quod non minus cumque,
+          recusandae hic soluta harum aut nulla...
+        </p>
+        <p><a class="gray" href="#">View all 2 comments</a></p>
+        <input type="text" placeholder="Add a comments...">
+
+      </div>
+    </div>
+    <div class="modal fade" id="post-modal" tabindex="-1" aria-labelledby="postModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-fullscreen-md-down modal-dialog-centered modal-dialog-scrollable modal-xl">
+        <div class="modal-content">
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-md-6">
+                <img src="https://i.ibb.co/Jqh3rHv/img1.jpg" class="img-fluid rounded-3" alt="Post Image">
+              </div>
+              <div class="col-md-6">
+                <div class="post">
+                  <div class="info">
+                    <div class="person" data-bs-toggle="modal" data-bs-target="#post-modal">
+                      <img src="https://i.ibb.co/3S1hjKR/account1.jpg">
+                      <a href="#">zineb</a>
+                      <span class="circle">.</span>
+                      <a>45m</a>
+                    </div>
+                    <div class="more">
+                      <img src="./images/show_more.png" alt="show more">
+                    </div>
+                  </div>
+                  <div class="desc">
+                    <div class="icons">
+                      <div class="icon_left d-flex">
+                        <div class="like">
+                          <div class="like">
+                            <img class="not_loved" src="./images/love.png" alt="">
+                            <img class="loved" src="./images/heart.png" alt="">
+                            <p> 55</p>
+                          </div>
+                          <img class="loved" src="./images/heart.png">
+                        </div>
+                        <div class="chat">
+                          <button type="button" class="btn p-0">
+                            <img src="./images/bubble-chat.png">
+                          </button>
+                        </div>
+                        <div class="send">
+                          <button type="button" class="btn p-0">
+                            <img src="./images/send.png">
+                          </button>
+                        </div>
+                      </div>
+                      <div class="save not_saved">
+                        <img class="hide saved" src="./images/save_black.png">
+                        <img class="not_saved" src="./images/save-instagram.png">
+                      </div>
+                    </div>
+                    <div class="liked">
+                      <a class="bold" href="#">150 likes</a>
+                    </div>
+                    <div class="post_desc">
+                      <p>
+                        <a class="bold" href="#">zineb</a>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima accusantium aperiam quod non
+                        minus cumque,
+                        recusandae hic soluta harum aut nulla...
+                      </p>
+                      <div class="comments">
+                        <div class="comment">
+                          <div class="d-flex">
+                            <div class="img">
+                              <img src="./images/profile_img.jpg" alt="">
+                            </div>
+                            <div class="content">
+                              <div class="person">
+                                <h4>namePerson</h4>
+                                <span>3j</span>
+                              </div>
+                              <p>Wow amzing shot</p>
+                              <div class="replay">
+                                <input type="text" placeholder="Add a reply...">
+                                <button class="translation">see translation</button>
+                              </div>
+                              <div class="answers">
+                                <button class="see_comment">
+                                  <span class="hide_com hide">Hide all responses</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="responses hide">
+                          <div class="response comment">
+                            <div class="d-flex">
+                              <div class="img">
+                                <img src="./images/profile_img.jpg" alt="">
+                              </div>
+                              <div class="content">
+                                <div class="person">
+                                  <h4>namePerson</h4>
+                                  <span>3j</span>
+                                </div>
+                                <p>Wow amzing shot</p>
+                                <div class="replay">
+                                  <input type="text" placeholder="Add a reply...">
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <input type="text" placeholder="Add a comments...">
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
