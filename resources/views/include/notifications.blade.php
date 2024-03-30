36cbee05b1dfade113ed71af5fa5ae68a248bc51
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-03-30 17:33:50 +0200

    feat(marwan): added notification component to include folders

---
+++ resources/views/include/notifications.blade.php
@@ -0,0 +1,129 @@
+<div id="notification" class="notification_section">
+    <h2>Notifications</h2>
+    <div class="notifications">
+        <div class="notif follow_notif">
+            <div class="cart">
+                <div>
+                    <div class="img">
+                        <img src="./images/profile_img.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="name">
+                            Zineb_essoussi
+                            <span class="desc">started following you.</span>
+                            <span class="time">2h</span>
+                        </p>
+
+                    </div>
+                </div>
+                <div class="follow_you">
+                    <button class="follow_text">Follow</button>
+                </div>
+            </div>
+        </div>
+        <div class="notif follow_notif">
+            <div class="cart">
+                <div>
+                    <div class="img">
+                        <img src="./images/profile_img.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="name">
+                            Zineb_essoussi
+                            <span class="desc">started following you.</span>
+                            <span class="time">2h</span>
+                        </p>
+
+                    </div>
+                </div>
+                <div class="follow_you">
+                    <button class="follow_text">Follow</button>
+                </div>
+            </div>
+        </div>
+        <div class="notif story_notif">
+            <div class="cart">
+                <div>
+                    <div class="img">
+                        <img src="./images/profile_img.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <div class="info">
+                            <p class="name">
+                                Zineb_essoussi
+                                <span class="desc">liked your story.</span>
+                                <span class="time">2d</span>
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="story_like">
+                    <img src="./images/img2.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="notif follow_notif">
+            <div class="cart">
+                <div>
+                    <div class="img">
+                        <img src="./images/profile_img.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="name">
+                            Zineb_essoussi
+                            <span class="desc">started following you.</span>
+                            <span class="time">2h</span>
+                        </p>
+
+                    </div>
+                </div>
+                <div class="follow_you">
+                    <button class="follow_text">Follow</button>
+                </div>
+            </div>
+        </div>
+        <div class="notif story_notif">
+            <div class="cart">
+                <div>
+                    <div class="img">
+                        <img src="./images/profile_img.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <div class="info">
+                            <p class="name">
+                                Zineb_essoussi
+                                <span class="desc">liked your story.</span>
+                                <span class="time">2d</span>
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="story_like">
+                    <img src="./images/img2.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="notif follow_notif">
+            <div class="cart">
+                <div>
+                    <div class="img">
+                        <img src="./images/profile_img.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="name">
+                            Zineb_essoussi
+                            <span class="desc">started following you.</span>
+                            <span class="time">2h</span>
+                        </p>
+
+                    </div>
+                </div>
+                <div class="follow_you">
+                    <button class="follow_text">Follow</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
