bb42fcfb6064fe784c4c81bae2f0cbfe8321643e
Zeinab Abdelghaffar <zeinanabdo88@gmail.com>
2024-03-31 10:33:01 +0200

    User Profile / Visited Profile / Edit

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/profile', function () {
-    return view('profile.profile');
-});
-Route::get('/edit-profile', function () {
-    return view('profile.edit-profile');
-});
-Route::get('/visit-profile', function () {
-    return view('profile.visit-profile');
-});
 
 
 // Route::get('/dashboard', function () {
