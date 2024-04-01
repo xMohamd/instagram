bcfc7f15337bfd071a69631ee8794b351818c949
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-04-01 16:15:04 +0200

    style(marwan): changed the icon of nav menu

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="./css/navMenu.css">
     <link rel="stylesheet" href="./sass/vender/bootstrap.css">
     <link rel="stylesheet" href="./sass/vender/bootstrap.min.css">
     <link rel="stylesheet" href="./owlcarousel/owl.theme.default.min.css">
