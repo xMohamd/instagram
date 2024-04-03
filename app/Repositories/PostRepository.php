1c1bda52cf2f95d98051fccf3f22a9d0a4bb1ac3
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-04-04 00:04:05 +0200

    Refactor PostRepository to include new methods for liking, unliking, commenting, and searching posts

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -4,34 +4,44 @@ namespace App\Repositories;
 
 use App\Contracts\RepositoryContract;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostRepository implements RepositoryContract
 {
     public function all()
     {
-
-        return  Post::with([
-            'user',
-            'likedByUsers',
-            'comments',
-            'media',
-            'comments.user',
-        ])
-            ->withCount('likedByUsers', 'comments')
+        return Post::whereIn('user_id', function ($query) {
+            $query->select('followed_id')
+                ->from('follows')
+                ->where('follower_id', Auth::id());
+        })
+            ->with([
+                'user',
+                'likes',
+                'user',
+                'comments.user',
+                'media',
+            ])
+            ->withCount(['likes', 'comments'])
             ->latest()
             ->get();
     }
 
     public function paginate($perPage = 10)
     {
-        return Post::with([
-            'user',
-            'likedByUsers',
-            'comments',
-            'media',
-            'comments.user',
-        ])
-            ->withCount('likedByUsers', 'comments')
+        return Post::whereIn('user_id', function ($query) {
+            $query->select('followed_id')
+                ->from('follows')
+                ->where('follower_id', Auth::id());
+        })
+            ->with([
+                'user',
+                'likes',
+                'user',
+                'comments.user',
+                'media',
+            ])
+            ->withCount(['likes', 'comments'])
             ->latest()
             ->simplePaginate($perPage);
     }
@@ -55,5 +65,38 @@ class PostRepository implements RepositoryContract
     public function delete($id)
     {
         return Post::destroy($id);
+    }
+
+    public function like($id)
+    {
+        $post = Post::findOrFail($id);
+        return $post->likes()->attach(Auth::id());
+    }
+
+    public function unlike($id)
+    {
+        $post = Post::findOrFail($id);
+        return $post->likes()->detach(Auth::id());
+    }
+
+    public function comment($id, $data)
+    {
+        $post = Post::findOrFail($id);
+        return $post->comments()->create($data);
+    }
+
+    public function search($query)
+    {
+        return Post::where('caption', 'like', "%$query%")
+            ->with([
+                'user',
+                'likes',
+                'user',
+                'comments.user',
+                'media',
+            ])
+            ->withCount(['likes', 'comments'])
+            ->latest()
+            ->get();
     }
 }
