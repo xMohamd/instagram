4a287a74322c624d4f9661e69939417d892046c9
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-04-08 05:30:09 +0200

    Refactor post component

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -41,7 +41,7 @@
         <button class="p-0 bg-transparent border-0 btn" data-post-save="{{ $post->id }}">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
             class="bi bi-bookmark-fill" viewBox="0 0 16 16">
-            <path
+            <path class="{{ $isSavedByUser }}"
               d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2" />
           </svg>
         </button>
