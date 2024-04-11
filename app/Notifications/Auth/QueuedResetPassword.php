a051247f97a3d1bd0b516db7be6d898e769d014b
Mohamed Hossny <mhossny2@outlook.com>
2024-04-11 11:38:34 +0200

    feat: all email sending is now processed through laravel queues

---
+++ app/Notifications/Auth/QueuedResetPassword.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Notifications\Auth;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class QueuedResetPassword extends ResetPassword implements ShouldQueue
+{
+    use Queueable;
+}
