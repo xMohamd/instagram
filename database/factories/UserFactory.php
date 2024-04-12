65d921467eb5a7135210e4e1ff0b21185b3432ce
MohamedAliEsmaill <mohamedali.official2020@gmail.com>
2024-04-12 14:27:24 +0200

    fix : resolve conflicts

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,11 +29,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-<<<<<<< Updated upstream
-=======
             'is_admin' => fake()->boolean(),
             'avatar' => $this->faker->imageUrl(200, 200, 'people'),
->>>>>>> Stashed changes
         ];
     }
 
